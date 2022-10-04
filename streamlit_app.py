bd9795213e0ba4ed2c2905e35ca761c4334590b8
ryanskelly42 <111290249+ryanskelly42@users.noreply.github.com>
2022-10-04 11:20:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,17 +26,18 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+#new section to display fruityvice api response 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+    
 streamlit.write('The user entered ', fruit_choice)
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-
-# takes the JSON and normalises in 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# outputs normalised JSON to dataframe
-streamlit.dataframe(fruityvice_normalized)
 
 #dont run anything past here while troubleshooting
 streamlit.stop()
@@ -50,3 +51,6 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','')
 my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
 streamlit.write('Thank you for adding ', add_my_fruit)
+
+
+
